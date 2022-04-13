05cb02b8e0d3543513a288b27695ca39e6e27c74
Bulent Akgul <bulent.akgul@me.com>
2022-04-13 16:56:56 +0300

    Initial commit

---
+++ src/PackagifyServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Bakgul\Packagify;
+
+use Illuminate\Support\ServiceProvider;
+
+class PackagifyServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+        //
+    }
+
+    public function register()
+    {
+        //
+    }
+}
