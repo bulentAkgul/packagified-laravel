d19fba4ede139bb2a42abdff1893ecf9d5e27709
Bulent Akgul <bulent.akgul@me.com>
2022-05-17 09:52:21 +0300

    bug fix: package path, methods extracted

--- src/Services/PackagifyService.php
+++ src/Services/PackagifyService.php
@@ -2,19 +2,36 @@
 
 namespace Bakgul\Packagify\Services;
 
+use Bakgul\Kernel\Helpers\Folder;
+use Bakgul\Kernel\Helpers\Settings;
 use Bakgul\Packagify\Functions\MakeFolder;
 use Bakgul\Packagify\Services\PackagifyServices\AppService;
 use Bakgul\Packagify\Services\PackagifyServices\ClientService;
-use Bakgul\Packagify\Services\PackagifyServices\PackageService;
 
 class PackagifyService
 {
     public static function create(): void
-    {
-        MakeFolder::_(base_path('resources'));
+    {        
+        self::resources();
+        self::packages();
 
-        PackageService::create();
         AppService::create();
         ClientService::create();
+    }
+
+    private static function resources()
+    {
+        file_exists(base_path('resources'))
+            ? array_map(fn ($x) => unlink($x), Folder::files(base_path('resources')))
+            : MakeFolder::_(base_path('resources'));
+    }
+
+    private static function packages()
+    {
+        if (Settings::standalone()) return;
+
+        foreach (Settings::roots() as $root) {
+            MakeFolder::_(base_path(Settings::folders('packages')), $root['folder']);
+        }
     }
 }
